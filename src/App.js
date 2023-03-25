ea1fdbb65cf959ef03824384208314c5433eaa57
jpcnlss <123640714+jpcnlss@users.noreply.github.com>
2023-03-25 11:02:45 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import './App.css';
 
 function Button({label, onClick}) {
   return (
-    <button className="Calculator" onClick={onClick}>
+    <button className="CalculatorBtn" onClick={onClick}>
 	{label}
     </button>
   );
 }
+
+
   
 function Display({display}) {
   return (
@@ -23,67 +25,101 @@ function Display({display}) {
 function App() {
 	
 	const[disp, setDisp] = useState(0);
+	const[num1, setNum1] = useState(null);
+	const[oper, setOper] = useState(null);
+	const[num2, setNum2] = useState(null);
 	
 	const numberClickHandler = (e) => {
 		e.preventDefault();
+		
 		const value = e.target.innerHTML;
-	
-		console.log(value);
-		setDisp(value);
-	
+		if(oper === null){
+			if(num1 === null){
+				setNum1(value);
+				setDisp(value);
+			} else {
+				setNum1(num1+value);
+				setDisp(num1+value);
+			}
+		} else {
+			if(num2 === null){
+				setNum2(value);
+				setDisp(value);
+			} else {
+				setNum2(num2+value);
+				setDisp(num2+value);
+			}
+			
+		console.log(num1 + " " + oper + " " + num2);
+		}
+		
 	}
 	
 	const operatorClickHandler = (e) => {
 		e.preventDefault();
 		const value = e.target.innerHTML;
-		
-		console.log(value);
+		setOper(value);
 		setDisp(value);
 		
+		console.log(num1 + " " + oper + " " + num2);
 	}
 	
 	const equalClickHandler = (e) => {
 		e.preventDefault();
-		const value = e.target.innerHTML;
 		
-		console.log(value);
-		setDisp(value);
-	
+		if (oper === "+") {
+			setDisp(parseInt(num1) + parseInt(num2));
+		}
+		else if (oper === "-") {
+			setDisp(parseInt(num1) - parseInt(num2));
+		}
+		else if (oper === "*") {
+			setDisp(parseInt(num1) * parseInt(num2));
+		}
+		else if (oper === "/") {
+			setDisp(parseInt(num1) / parseInt(num2));
+		} else { 
+			setDisp("ERROR");
+		}
+
+		
 	}
 	
 	const clearClickHandler = (e) => {
 		e.preventDefault();
-		const value = e.target.innerHTML;
 		
-		console.log(value);
-		setDisp(value);
+		
+		
+		setDisp(0);
+		setNum1(null);
+		setNum2(null);
+		setOper(null);
 	}
 	
   return (
   <div className="Calculator">
-	
-	<Display display={0}/>
-	<div className={"ButtonContainer"}>
-		<Button label={7} onClick={numberClickHandler}/>
-		<Button label={8} onClick={numberClickHandler}/>
-		<Button label={9} onClick={numberClickHandler}/>
-		<Button label={"+"} onClick={operatorClickHandler}/>
-		<Button label={4} onClick={numberClickHandler}/>
-		<Button label={5} onClick={numberClickHandler}/>
-		<Button label={6} onClick={numberClickHandler}/>
-		<Button label={"-"} onClick={operatorClickHandler}/>
-		<Button label={1} onClick={numberClickHandler}/>
-		<Button label={2} onClick={numberClickHandler}/>
-		<Button label={3} onClick={numberClickHandler}/>
-		<Button label={"*"} onClick={operatorClickHandler}/>
-		<Button label={"C"} onClick={clearClickHandler}/>
-		<Button label={0} onClick={numberClickHandler}/>	
-		<Button label={"="} onClick={equalClickHandler}/>
-		<Button label={"÷"} onClick={operatorClickHandler}/>
-		
-		
-		</div>
-   </div>
+  <div className="calculatorName">JPCNLSS BS-CPE3A</div>
+    <Display display={disp}/>
+	<div className="ButtonContainer">
+      <Button label={7} onClick={numberClickHandler}/>
+      <Button label={8} onClick={numberClickHandler}/>
+      <Button label={9} onClick={numberClickHandler}/>
+      <Button label={"/"} onClick={operatorClickHandler}/>
+      <Button label={4} onClick={numberClickHandler}/>
+      <Button label={5} onClick={numberClickHandler}/>
+      <Button label={6} onClick={numberClickHandler}/>
+      <Button label={"*"} onClick={operatorClickHandler}/>
+      <Button label={1} onClick={numberClickHandler}/>
+      <Button label={2} onClick={numberClickHandler}/>
+      <Button label={3} onClick={numberClickHandler}/>
+      <Button label={"-"} onClick={operatorClickHandler}/>
+      <Button label={"C"} onClick={clearClickHandler}/>
+      <Button label={0} onClick={numberClickHandler}/>
+      <Button label={"="} onClick={equalClickHandler}/>
+      <Button label={"+"} onClick={operatorClickHandler}/>
+     </div>
+    </div>
+ 
     
   );
 }
